0b67eac39fbf96ff375979fc47cc28efabbdcc9c
Fenrir <corbinjnelson@gmail.com>
2019-11-14 18:08:15 -0500

    added follow script

---
+++ Assets/Scenes/Ink_test/Scripts/Follow.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+public class Follow : MonoBehaviour
+{
+    public GameObject Player;
+    public float movementSpeed = 3;
+    void Update()
+    {
+        transform.LookAt(Player.transform);
+        transform.position += transform.forward * movementSpeed * Time.deltaTime;
+    }
+}
